c3165a5f646a49fc7ba8d5f5126f42e82d55946b
Samaresh Sridhar <ssamaresh1990@gmail.com>
2020-12-30 23:55:20 -0500

    - added new query

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # apollo-server-netlify-lambda-starter
 A starter project that creates a graphql server and can be deployed to netlify for public access
+
+## Installation
+
+Clone repository and run npm install to install the dependencies.
+
+```bash
+npm install
+```
+
+### Start locally
+
+To run the project locally, use the `start` command.
+
+```bash
+npm run start
+```
--- src/schema.js
+++ src/schema.js
@@ -2,13 +2,15 @@ const {gql} = require('apollo-server-lambda');
 
 const typeDefs = gql`
   type Query {
-    hello: String    
+    hello: String
+    goodBye: String 
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hi there! This is your only resolver.'
+    hello: () => 'Hi there! This is your only resolver.',
+    goodBye: () => 'Hi there! Time to say goodbye!'
   }
 };
 
